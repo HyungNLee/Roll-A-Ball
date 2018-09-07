c0e8f7f570fb8c62dc7dd4cbb009757b115b175e
Hyung Lee <hyungnaelee@gmail.com>
2018-09-06 23:01:03 -0700

    Finish step 3. Attach camera to ball in offset position through CameraController script.

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	//public GameObject reference to the player.
+	public GameObject player;
+
+	//private Vector3 offset value for camera relative to player.
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame like Update but guaranteed to run after all items have been processed in Update. Camera position is best used in LateUpdate.
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
