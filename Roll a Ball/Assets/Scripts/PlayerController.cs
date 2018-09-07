346c0571c1b404e1e3d3291487c6fef8255b49ac
Hyung Lee <hyungnaelee@gmail.com>
2018-09-06 22:49:30 -0700

    Finish step 2. Attach script to Rigidbody component to make the ball move using WASD keys.

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// used to control speed. By making this variable public this variable will show up in the inspector as a editable property. 
+	public float speed;
+
+	//variable used to hold the Rigidbody component reference.
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Called before performing any physics compilations
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+
+		// Vector3 is (x, y, z)
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce(movement * speed);
+	}
+}
