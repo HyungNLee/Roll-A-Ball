1d777c9d865648cc8239b7791393e6b1d1eb570a
Hyung Lee <hyungnaelee@gmail.com>
2018-09-07 14:42:13 -0700

    Finish step 8. Create UI for count text and win text. Build an exe in 'builds' folder.

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	// used to control speed. By making this variable public this variable will show up in the inspector as a editable property. 
 	public float speed;
 
+  //Used for UI count text
+  public Text countText;
+
+  //Used to show win text
+  public Text winText;
+
 	//variable used to hold the Rigidbody component reference.
 	private Rigidbody rb;
+
+  //Used to hold collectible count.
+  private int count;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
+    count = 0;
+    SetCountText();
+    winText.text = "";
 	}
 
 	// Called before performing any physics compilations
@@ -33,6 +46,17 @@ public class PlayerController : MonoBehaviour {
   	if (other.gameObject.CompareTag("Pick Up"))
 		{
 			other.gameObject.SetActive(false);
+      count++;
+      SetCountText();
 		}
+  }
+
+  void SetCountText()
+  {
+    countText.text = "Count: " + count.ToString();
+    if (count >= 12)
+    {
+      winText.text = "You win!";
+    }
   }
 }
